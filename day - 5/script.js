c30063d9cccfed4814b70b32d56b83bab30da3c7
Dinesh Kumar S <dineshkumardk2k6@gmail.com>
2025-12-08 23:22:20 +0530

    day - 5 work

---
+++ day - 5/script.js
@@ -0,0 +1,8 @@
+document.body.style.backgroundColor ="orange";
+document.body.style.color = "black";
+const uname = "john_doe";
+
+document.body.innerHTML += "<h2>Welcome to JavaScript Programming!</h2>";
+document.body.style.fontSize = "20px";
+
+document.title = `${uname}-dashboard`;
